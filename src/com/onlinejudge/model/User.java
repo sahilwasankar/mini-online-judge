da347defc266e5a4155e885c5a31ac7d85f196c9
sahilwasankar <sahilwasankar777@gmail.com>
2026-02-25 15:51:47 +0530

    Use Role enum instead of String in User model

--- src/com/onlinejudge/model/User.java
+++ src/com/onlinejudge/model/User.java
@@ -4,9 +4,9 @@ public class User {
     private int id ;
     private String username;
     private String password;
-    private String role;
+    private Role role;
 
-public User (int id, String username, String password , String role) {
+public User (int id, String username, String password , Role role) {
 
     this.id = id ;
     this.username =username;
@@ -24,7 +24,7 @@ public String getPassword(){
     return password;
 
 }
-public String getRole(){
+public Role getRole(){
     return role;
 }
 }
