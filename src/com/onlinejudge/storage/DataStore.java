ca4c3661eb735d2c871fa6dce0ab5bded840088d
sahilwasankar <sahilwasankar777@gmail.com>
2026-02-26 18:05:26 +0530

    Add static users list in DataStore

---
+++ src/com/onlinejudge/storage/DataStore.java
@@ -0,0 +1,9 @@
+package com.onlinejudge.storage;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.onlinejudge.model.User;
+
+public class DataStore {
+     public static List<User> users = new ArrayList<>();
+}
